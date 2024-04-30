eae312a949abb02457b1136c48c9503a57c38766
John Jomar <johndjomar@gmail.com>
2024-05-01 03:38:58 +0800

    Added main flow

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
+
+    static String input;
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    static Parser parser;
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        parser = new Parser();
+        
+        while (true) {
+            try {
+                System.out.print("\nEnter your Java Variable Initialization/Declaration: ");
+                input = reader.readLine();
+                parser.parseStatement(input);
+                
+            } catch (Exception e) {
+                System.out.println("Invalid Input");
+                System.out.println(e);
+            }
+        }
     }
 }
