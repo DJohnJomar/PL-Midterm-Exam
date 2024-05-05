e0480745b4ea22a493a12b2742916e4d3448e2e1
John Jomar <johndjomar@gmail.com>
2024-05-05 16:00:58 +0800

    Re added printing of error

--- src/Main.java
+++ src/Main.java
@@ -26,6 +26,7 @@ public class Main {
                 
             } catch (Exception e) {
                 System.out.println("----------Invalid Input!----------");
+                System.out.println(e);
                 parser.clearResult();//Result clearing functions when unexpected errors arise
             }
         }
